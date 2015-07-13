95b33df27121993e56befdf67155ae50ce53d132
Kaan Tekelioglu <buluttekelioglu@gmail.com>
2015-07-13 16:58:45 +0300

    First Implementation
    The two methods are implemented. Seems to be working for case [[1, 2],
    [3, 4]].

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a matrix object whose inverse can be cached.
+## cacheSolve returns the inverse of the cacheMatrix object, 
+## making use of the cache if the inverse is known.
 
-## Write a short comment describing this function
+## Creates a cacheMatrix object with four functions: $set, $get, $setinv and $getinv
+## If known, the inverse of the matrix can be supplied as a parameter.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(), inverse = NULL) {
+        inv <- inverse
+        set <- function(y, inverse = NULL) {
+            x <<- y
+            inv <<- inverse
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+    
 }
 
 
-## Write a short comment describing this function
+## Returns a cacheMatrix object which is the inverse of the given matrix.
+## The inverse of the returned matrix is known and cached.
+## The cache is returned unless it is NULL.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    
+    if(!is.null(inv)){
+        message("Getting cached data")
+        return(inv)
+    }
+    
+    inv <- solve(x$get(), ...)
+    matrInv <- makeCacheMatrix(inv,x)
+    x$setinv(matrInv)
+    
+    matrInv
 }
